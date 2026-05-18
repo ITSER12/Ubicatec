be1bf229587f9c64eaa94aa89be0cc649aa2fcd0
itser <juegosctg@gmail.com>
2026-05-18 16:23:13 -0600

    yutub4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,23 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('apartado_aulas', function (Blueprint $table) {
-
-            try {
-                $table->dropForeign(['user_id']);
-            } catch (\Exception $e) {
-                //
-            }
 
             $table->foreign('user_id')
                 ->references('no_control')
                 ->on('usuarios')
                 ->nullOnDelete();
+
         });
     }
 };
